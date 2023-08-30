38b2607b82ec27b8d07eb15fe0496d4e46044851
Elshan Gurbanov <elshan.elshan.82@gmail.com>
2023-08-31 01:56:18 +0300

    fix

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from modules.Board import Board
-from modules.Utils import FENParser
-import pprint
+# from modules.Board import Board
+# from modules.Utils import FENParser
+# import pprint
+# from modules.BoardASCIIRenderer import BoardASCIIRenderer
 
 
-board = Board()
-pprint.pprint(board.board)
+# board = Board("r1b1kb1r/p1ppqp1p/1pn2n2/4p1p1/2BPP3/2N2N2/PPP2PPP/R1BQK2R w KQkq - 0 1")
+# renderer = BoardASCIIRenderer()
+# print(renderer.render(board.board))
